8204dfc8890dbc82af1d5f78d516dbd384866639
mamunur105 <rmamunur105@gmail.com>
2020-11-08 02:43:03 +0600

    some change

--- basic-slider.php
+++ basic-slider.php
@@ -1,8 +1,15 @@
 <?php
 /**
+ * The plugin bootstrap file
+ *
+ * @link              https://wordpress.org/plugins/wc-basic-slider
+ * @since             1.0.1
+ * @package           BSFW_Slider
+ *
+ * @wordpress-plugin
  * Plugin Name:       Basic Slider for WooCommerce
  * Plugin URI:        https://wordpress.org/plugins/wc-basic-slider
- * Description:       This is for woocommerce Category,Related product, And also For Promotional basic slider. 
+ * Description:       This is for woocommerce Category,Related product, And also For Promotional basic slider.
  * Version:           1.0.1
  * Requires at least: 5.0
  * Requires PHP:      7.0
@@ -15,59 +22,68 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
-// use Carbon_Fields;
-require_once __DIR__.'/vendor/autoload.php';  
-
-final class BSFW_Slider{
-
-   /**
-	* Plugin version 
-	*/
-	const version = '1.0.1';
+/**
+ * Autoload all file
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+/**
+ * Plugin main class
+ */
+final class BSFW_Slider {
 
 	/**
-	 * class constructr 
+	 * Plugin version
 	 *
+	 * @var   string
 	 */
-	private function __construct(){
+	public const VERSION = '1.0.1';
+
+	/**
+	 * Constructr
+	 */
+	private function __construct() {
 
 		$this->define_constant();
-		new BasicSliderForWooCommerce\BSCpt(); 
-		// new Basic\Slider\BsOptions();
-		if (is_admin()) {
-			new BasicSliderForWooCommerce\Admin();	
+		new BasicSliderForWooCommerce\BSCpt();
+		if ( is_admin() ) {
+			new BasicSliderForWooCommerce\Admin();
 		}
-
-		register_activation_hook(__FILE__,[ $this,'activate' ]);
-		add_action('init',[ $this,'init_plugin' ]);
-		add_action( 'plugins_loaded', [ $this , 'load_textdomain'] );
-		add_action( 'after_setup_theme', 'bsfw_crb_load',99 );
-		add_action( 'carbon_fields_register_fields','bsfw_slider_attach_post_meta' );
-		add_action( 'carbon_fields_register_fields','bsfw_slider_settings' );
-		add_action( 'after_setup_theme', [$this,'after_setup_theme']);
-
-		add_action( 'admin_notices', [$this,'bs_slider_notice_message'] );
-		// add_filter('','<');
-
+		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		add_action( 'init', array( $this, 'init_plugin' ) );
+		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+		add_action( 'after_setup_theme', 'bsfw_crb_load', 99 );
+		add_action( 'carbon_fields_register_fields', 'bsfw_slider_attach_post_meta' );
+		add_action( 'carbon_fields_register_fields', 'bsfw_slider_settings' );
+		add_action( 'after_setup_theme', array( $this, 'after_setup_theme' ) );
+		add_action( 'admin_notices', array( $this, 'bs_slider_notice_message' ) );
 	}
-
-  	function load_textdomain() {
-		load_plugin_textdomain( 'bs-slider', false, plugin_dir_path( __FILE__ ) . "languages" );
+	/**
+	 * Lode textdomain
+	 *
+	 * @return void
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'bs-slider', false, plugin_dir_path( __FILE__ ) . 'languages' );
 	}
-
-	function after_setup_theme(){
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public function after_setup_theme() {
 		add_image_size( 'custom-size', 220, 180, true );
 	}
 	/**
-	 * initilize a singleton instance
-	 * @return \WD_ac
+	 * Initilize a singleton instance
+	 *
+	 * @return \BSFW_Slider
 	 */
-	public static function init(){
+	public static function init() {
 		static $instance = false;
-		if (!$instance) {
+		if ( ! $instance ) {
 			$instance = new self();
 		}
 		return $instance;
@@ -75,36 +91,47 @@ final class BSFW_Slider{
 
 	/**
 	 *  Requred constant
-	 * @return \WD_ac
+	 *
+	 * @return void
 	 */
-	public function define_constant(){
-		define('BSFW_VERSION', self::version);
-		define('BSFW_FILE', __FILE__);
-		define('BSFW_PATH', __DIR__);
-		define('BSFW_URL', plugins_url('',BSFW_FILE));
-		define('BSFW_ASSETS', BSFW_URL.'/assets');
-	
+	public function define_constant() {
+		define( 'BSFW_VERSION', self::VERSION );
+		define( 'BSFW_FILE', __FILE__ );
+		define( 'BSFW_PATH', __DIR__ );
+		define( 'BSFW_URL', plugins_url( '', BSFW_FILE ) );
+		define( 'BSFW_ASSETS', BSFW_URL . '/assets' );
+
 	}
 
 	/**
 	 *  Do stuff upon activation
-	 * @return 
+	 *
+	 * @return void
 	 */
-	public function activate(){ }
-
-	public function init_plugin(){ 
-		new BasicSliderForWooCommerce\Assets(); 
-
-		if (!is_admin()) {
-			new BasicSliderForWooCommerce\Frontend();	 
+	public function activate() {
+		flush_rewrite_rules( false );
+	}
+	/**
+	 * Init Function
+	 *
+	 * @return void
+	 */
+	public function init_plugin() {
+		new BasicSliderForWooCommerce\Assets();
+		if ( ! is_admin() ) {
+			new BasicSliderForWooCommerce\Frontend();
 		}
-		
 	}
 
-	public function bs_slider_notice_message( $type ) {
-		if ( !class_exists( 'woocommerce' ) ) {
+	/**
+	 * Notice
+	 *
+	 * @return void
+	 */
+	public function bs_slider_notice_message() {
+		if ( ! class_exists( 'woocommerce' ) ) {
 			echo '<div class="updated notice is-dismissible notice-sp-wcsp-woo"><p>';
-			echo __( 'Please active WooCommerce plugin to make the <b>Category Slider for WooCommerce</b>.', 'bs-slider' );
+			_e( 'Please active WooCommerce plugin to make the <b>Category Slider for WooCommerce</b>.', 'bs-slider' );
 			echo '</p></div>';
 		}
 	}
@@ -112,8 +139,12 @@ final class BSFW_Slider{
 
 
 }
-
-function bsfw_slider(){
+/**
+ * Activation slider
+ *
+ * @return BSFW_Slider
+ */
+function bsfw_slider() {
 	return BSFW_Slider::init();
 }
 
