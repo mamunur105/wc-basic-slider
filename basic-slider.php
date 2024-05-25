7de951b7e06b3e1bae3b9101b0dea03bb95003dc
Mamunur Rashid <rmamunur105@gmail.com>
2024-05-25 13:49:10 +0600

    Fied

--- basic-slider.php
+++ basic-slider.php
@@ -71,3 +71,13 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 		$plugin->run();
 	}
 );
+
+// HPOS.
+add_action(
+	'before_woocommerce_init',
+	function () {
+		if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+		}
+	}
+);
